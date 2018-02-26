e7d2c63824e1875038a3f5c781a5f432c7376dc1
Marianne Pietschnig <mp586@exeter.ac.uk>
2018-02-26 18:07:26 +0000

    Minor changes to lepref07 co2 .py

--- exp/mp586/constant_evaporative_resistance/full_continents_lepref07_fulllepref07qflux_plus_2xCO2_361_run.py
+++ exp/mp586/constant_evaporative_resistance/full_continents_lepref07_fulllepref07qflux_plus_2xCO2_361_run.py
@@ -201,7 +201,7 @@ exp.namelist = namelist = Namelist({
 
 #Lets do a run!
 exp.run(1, use_restart=False, num_cores=NCORES)
-for i in range(2,361):
+for i in range(2,362):
     exp.run(i, num_cores=NCORES)
 
 
