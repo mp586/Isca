dc7a3a4c72a8e6b2a0ab97dab2449bd7ae4c1733
Marianne Pietschnig <mp586@exeter.ac.uk>
2018-05-10 15:50:43 +0100

    updated commit number in two continents newbucket with prefactor exp just in case

--- exp/mp586/bucket/co2_forcing/two_continents_newbucket_with_prefactor08_finalIscaAPqflux_landqfluxzero_and_2xCO2.py
+++ exp/mp586/bucket/co2_forcing/two_continents_newbucket_with_prefactor08_finalIscaAPqflux_landqfluxzero_and_2xCO2.py
@@ -10,7 +10,7 @@ base_dir = os.getcwd()
 # useful for iterative development
 #cb = IscaCodeBase.from_directory(GFDL_BASE)
 
-cb = IscaCodeBase.from_repo(repo='https://github.com/mp586/Isca.git', commit='5fdaad7')
+cb = IscaCodeBase.from_repo(repo='https://github.com/mp586/Isca.git', commit='72a499b')
 # or it can point to a specific git repo and commit id.
 # This method should ensure future, independent, reproducibility of results.
 # cb = DryCodeBase.from_repo(repo='https://github.com/isca/isca', commit='isca1.1')
