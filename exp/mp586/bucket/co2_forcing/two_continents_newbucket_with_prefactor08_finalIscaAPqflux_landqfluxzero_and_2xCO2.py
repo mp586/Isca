72a499bbd6b34190201efcf227dc2addad78bfb1
Marianne Pietschnig <mp586@exeter.ac.uk>
2018-05-10 15:03:32 +0100

    Corrected restart file path , bracket was missing

--- exp/mp586/bucket/co2_forcing/two_continents_newbucket_with_prefactor08_finalIscaAPqflux_landqfluxzero_and_2xCO2.py
+++ exp/mp586/bucket/co2_forcing/two_continents_newbucket_with_prefactor08_finalIscaAPqflux_landqfluxzero_and_2xCO2.py
@@ -396,6 +396,6 @@ exp.namelist = namelist = Namelist({
 })
 
 #Lets do a run!
-exp.run(1, restart_file=os.path.join(GFDL_DATA,'two_continents_newbucket_with_prefactor08_finalIscaAPqflux_landqfluxzero/restarts/res0361.tar.gz', num_cores=NCORES)
+exp.run(1, restart_file=os.path.join(GFDL_DATA,'two_continents_newbucket_with_prefactor08_finalIscaAPqflux_landqfluxzero/restarts/res0361.tar.gz'), num_cores=NCORES)
 for i in range(2,481):
     exp.run(i, num_cores=NCORES)
