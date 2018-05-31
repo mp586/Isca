ad377ef95f688761e00fa34ef41979c154295604
Marianne Pietschnig <mp586@exeter.ac.uk>
2018-05-31 16:59:11 +0100

    added missing bracket!

--- exp/mp586/bucket/full_continents_newbucket_2xCO2qflux_2xCO2_spinup361_from_fullnewbucketqflux.py
+++ exp/mp586/bucket/full_continents_newbucket_2xCO2qflux_2xCO2_spinup361_from_fullnewbucketqflux.py
@@ -210,7 +210,7 @@ exp.namelist = namelist = Namelist({
 })
 
 # #Lets do a run!
-exp.run(1, restart_file=os.path.join(GFDL_DATA,'full_continents_newbucket_fullnewbucketqflux/restarts/res0361.tar.gz', num_cores=NCORES)
+exp.run(1, restart_file=os.path.join(GFDL_DATA,'full_continents_newbucket_fullnewbucketqflux/restarts/res0361.tar.gz'), num_cores=NCORES)
 for i in range(2,481):
     exp.run(i, num_cores=NCORES)
 
