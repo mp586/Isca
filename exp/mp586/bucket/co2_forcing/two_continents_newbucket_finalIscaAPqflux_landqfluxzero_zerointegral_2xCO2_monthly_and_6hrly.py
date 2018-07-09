92d43ccf0f4b5fe857e8030f3483f41afbbf690c
Marianne Pietschnig <mp586@exeter.ac.uk>
2018-07-09 15:27:45 +0100

    Updated commit to the one before this one so it accesses the right qflux file

--- exp/mp586/bucket/co2_forcing/two_continents_newbucket_finalIscaAPqflux_landqfluxzero_zerointegral_2xCO2_monthly_and_6hrly.py
+++ exp/mp586/bucket/co2_forcing/two_continents_newbucket_finalIscaAPqflux_landqfluxzero_zerointegral_2xCO2_monthly_and_6hrly.py
@@ -11,7 +11,7 @@ base_dir = os.getcwd()
 # cb = IscaCodeBase.from_directory(GFDL_BASE)
 
 
-cb = IscaCodeBase.from_repo(repo='https://github.com/mp586/Isca.git', commit='77a619d')
+cb = IscaCodeBase.from_repo(repo='https://github.com/mp586/Isca.git', commit='995905e')
 
 # or it can point to a specific git repo and commit id.
 # This method should ensure future, independent, reproducibility of results.
