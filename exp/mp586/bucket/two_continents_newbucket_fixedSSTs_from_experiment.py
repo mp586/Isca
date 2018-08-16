b53b20e57e59a823ccc4a0a70925fb22cb42a8e7
Marianne Pietschnig <mp586@exeter.ac.uk>
2018-08-16 18:14:57 +0100

    cb from remote

--- exp/mp586/bucket/two_continents_newbucket_fixedSSTs_from_experiment.py
+++ exp/mp586/bucket/two_continents_newbucket_fixedSSTs_from_experiment.py
@@ -8,7 +8,9 @@ NCORES = 16
 base_dir = os.getcwd()
 # a CodeBase can be a directory on the computer,
 # useful for iterative development
-cb = IscaCodeBase.from_directory(GFDL_BASE)
+# cb = IscaCodeBase.from_directory(GFDL_BASE)
+
+cb = IscaCodeBase.from_repo(repo='https://github.com/mp586/Isca.git', commit='7739582')
 
 # or it can point to a specific git repo and commit id.
 # This method should ensure future, independent, reproducibility of results.
