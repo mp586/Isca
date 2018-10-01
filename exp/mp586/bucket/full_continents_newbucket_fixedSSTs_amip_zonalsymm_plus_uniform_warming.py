fd712f7439700a4dafb1569852cb24d455a215d6
Marianne Pietschnig <mp586@exeter.ac.uk>
2018-10-01 16:05:13 +0100

    updated comit nr a second time

--- exp/mp586/bucket/full_continents_newbucket_fixedSSTs_amip_zonalsymm_plus_uniform_warming.py
+++ exp/mp586/bucket/full_continents_newbucket_fixedSSTs_amip_zonalsymm_plus_uniform_warming.py
@@ -10,7 +10,7 @@ base_dir = os.getcwd()
 # useful for iterative development
 # cb = IscaCodeBase.from_directory(GFDL_BASE)
 
-cb = IscaCodeBase.from_repo(repo='https://github.com/mp586/Isca.git', commit='9e0d484')
+cb = IscaCodeBase.from_repo(repo='https://github.com/mp586/Isca.git', commit='b3c4afc')
 
 # # or it can point to a specific git repo and commit id.
 # # This method should ensure future, independent, reproducibility of results.
