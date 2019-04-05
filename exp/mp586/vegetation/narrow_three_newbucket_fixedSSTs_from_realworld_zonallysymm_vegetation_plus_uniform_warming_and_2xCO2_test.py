5713d3b3d95eae16f1673e5c7fe87a6da65dfbdd
Marianne Pietschnig <mp586@exeter.ac.uk>
2019-04-05 02:03:12 +0100

    Updated commit # in narrow_three........

--- exp/mp586/vegetation/narrow_three_newbucket_fixedSSTs_from_realworld_zonallysymm_vegetation_plus_uniform_warming_and_2xCO2_test.py
+++ exp/mp586/vegetation/narrow_three_newbucket_fixedSSTs_from_realworld_zonallysymm_vegetation_plus_uniform_warming_and_2xCO2_test.py
@@ -10,7 +10,7 @@ base_dir = os.getcwd()
 # useful for iterative development
 # cb = IscaCodeBase.from_directory(GFDL_BASE)
 
-cb = IscaCodeBase.from_repo(repo='https://github.com/mp586/Isca.git', commit='')
+cb = IscaCodeBase.from_repo(repo='https://github.com/mp586/Isca.git', commit='7bb4387')
 
 # or it can point to a specific git repo and commit id.
 # This method should ensure future, independent, reproducibility of results.
